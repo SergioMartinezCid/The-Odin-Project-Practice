f25f8c10bea2ee4c13a1952d5ce4c4ef9736f9f4
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-09-13 18:48:54 +0200

    Add html/css/js basic structure

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,16 @@
+const gameBoard = () => {
+    const gameBoard = [
+        [undefined], [undefined], [undefined],
+        [undefined], [undefined], [undefined],
+        [undefined], [undefined], [undefined]
+    ],
+    return {};
+}
+
+const displayController = () => {
+    return {};
+}
+
+const playerFactory = () => {
+    return {};
+}
