b72fe1ebb2084dcdda5e57f4fd77a5da07f77582
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-09-13 10:40:52 +0200

    Set fixed size for the canvas

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -18,20 +18,18 @@ function createGrid(square_length){
     let root_div = document.createElement('div');
     root_div.id = 'root';
     root_div.style.display = 'grid';
-    root_div.style.gridTemplateColumns = `repeat(${square_length}, 2rem)`;
-    root_div.style.gridTemplateRows = `repeat(${square_length}, 2rem)`;
+    root_div.style.gridTemplateColumns = `repeat(${square_length}, 1fr)`;
+    root_div.style.gridTemplateRows = `repeat(${square_length}, 1fr)`;
     root_div.style.backgroundColor = 'AliceBlue';
     root_div.style.padding = '10px';
-    root_div.style.float = 'left';
-    root_div.style.overflow = 'auto';
+    root_div.style.width = '40rem';
+    root_div.style.height = '40rem';
     
     let children_divs = new Array(16);
     
     for(let i=0;i<square_length*square_length;i++){
         children_divs[i] = document.createElement('div');
-        children_divs[i].style.backgroundColor = 'white';
-        children_divs[i].style.textAlign = 'center';
-        children_divs[i].style.border = '1px solid cyan';    
+        children_divs[i].style.backgroundColor = 'white';  
         children_divs[i].addEventListener('mouseover', e => e.target.style.backgroundColor = 'black');
         root_div.appendChild(children_divs[i]);
     }
