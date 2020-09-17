0b3a6f96729cbf32dc6f4b9105698e3b2c1daeb2
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-09-17 11:01:36 +0200

    Structure the project interface

--- angular-library/src/app/app.module.ts
+++ angular-library/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppBodyComponent } from './app-body/app-body.component';
+import { AppBookAddComponent } from './app-book-add/app-book-add.component';
+import { AppBookTableComponent } from './app-book-table/app-book-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppBodyComponent,
+    AppBookAddComponent,
+    AppBookTableComponent
   ],
   imports: [
     BrowserModule
