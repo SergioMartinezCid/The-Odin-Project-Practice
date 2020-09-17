2fc86d5d260306714040bdbf6b489967c17ec3b4
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-09-17 11:38:07 +0200

    Implement app-book-table component

--- angular-library/src/app/app-book-table/app-book-table.component.ts
+++ angular-library/src/app/app-book-table/app-book-table.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { books } from '../domain/book';
 
 @Component({
   selector: 'app-app-book-table',
   templateUrl: './app-book-table.component.html',
   styleUrls: ['./app-book-table.component.css']
 })
-export class AppBookTableComponent implements OnInit {
 
+export class AppBookTableComponent implements OnInit {
+  books = books;
   constructor() { }
 
   ngOnInit(): void {
