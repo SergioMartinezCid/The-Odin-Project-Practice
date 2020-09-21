748f5def4651bfa8648096d967fceee8f3b051a1
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-09-21 18:05:12 +0200

    Create graphical components

--- battleship/src/app/app.module.ts
+++ battleship/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { GameSummaryComponent } from './game-summary/game-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    GameSummaryComponent
   ],
   imports: [
     BrowserModule
---
+++ battleship/src/app/game-summary/game-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-summary',
+  templateUrl: './game-summary.component.html',
+  styleUrls: ['./game-summary.component.css']
+})
+export class GameSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
