9d9d0e3997e7b3147b7edeae610798094cfaf100
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-09-28 10:32:31 +0200

    Attempt to deploy functional backend

--- where-s-waldo/functions/src/index.ts
+++ where-s-waldo/functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +9,57 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const startGame = functions.https.onCall((data, context) => {
+    if (!context.auth) return {status: 'error', code: 401, message: 'Not signed in'}
+
+    return new Promise(async (resolve, reject) => {
+        // Returns the subpath to the image, game token and starts the game on the backend
+        const imageEntry = await admin.firestore().collection('boardImages').doc(data.boardId).get();
+        if (imageEntry.data().subpath == null){
+            reject('boardId not found');
+        }
+
+        const newGame = await admin.firestore().collection('games').add(
+            {
+                start: admin.firestore.Timestamp.now(),
+                uid: context.auth.uid,
+            }
+        );
+        resolve(typeof imageEntry);
+        resolve({subpath: imageEntry.data().subpath, gameId : newGame.id})
+    });
+});
+
+export const cancelGame = functions.https.onCall((data, context) => {
+    if (!context.auth) return {status: 'error', code: 401, message: 'Not signed in'}
+
+    return new Promise(async (resolve, reject) => {
+        const entry = await admin.firestore().collection('games').doc(data.gameId).get();
+        if (entry == null){
+            reject('gameId not found');
+        }
+        if (context.auth?.uid !== entry.data().uid){
+            reject('Only the user who is playing that game can cancel it');
+        }
+        await admin.firestore().collection('games').doc(data.gameId).delete();
+    });
+});
+
+/*
+export const submitAnswer = functions.https.onCall((data, context) => {
+    if (!context.auth) return {status: 'error', code: 401, message: 'Not signed in'}
+
+    return new Promise((resolve, reject) => {
+
+    });
+});
+
+export const getRecords = functions.https.onCall((data, context) => {
+    if (!context.auth) return {status: 'error', code: 401, message: 'Not signed in'}
+
+    return new Promise((resolve, reject) => {
+
+    });
+})
+*/
--- where-s-waldo/public/scripts/main.js
+++ where-s-waldo/public/scripts/main.js
@@ -5,44 +5,11 @@ const imgWhereWaldo = document.querySelector('#img-where-waldo');
 const button_levels = document.querySelector('#button-levels');
 const button_login = document.querySelector('#button-login');
 const button_logout = document.querySelector('#button-logout');
+const char_select = document.querySelector('#char-select');
 
-async function fillBoardList(){
-    const querySnapshot = await firebase.firestore().collection('boards').get();
-    return querySnapshot.docs.map(doc => doc.data()).filter(board => !!board.isValid);
-}
-
-function generateBoardButtons(parentDiv, boardList){
-    boardList.forEach(board => {
-        const divLevel = document.createElement('button');
-        divLevel.innerText = board.name;
-        divLevel.addEventListener('click', async (event) => {
-            popup.style.display = 'none';
-            try {
-                imgWhereWaldo.src = await firebase.storage().ref().child('playable-boards')
-                                    .child(board.subpath).getDownloadURL();
-                } catch (error) {
-                alert(error.message);
-            }
-        });
-        parentDiv.appendChild(divLevel);
-    });
-}
-
-async function generateLevelSelector(){
-    popup.hidden = false;
-    const divLevels = document.createElement('div');
-    divLevels.classList.toggle('white-rectangle');
-    const subtitle = document.createElement('h2');
-    subtitle.innerText = 'Select one of the levels: ';
-    divLevels.appendChild(subtitle);
-
-    try {
-        generateBoardButtons(divLevels, await fillBoardList());
-    } catch (error) {
-        alert(error.message);
-    }
-    return divLevels;
-}
+let charSelTimeout = null;
+let gameStarted = false;
+let gameId = '';
 
 function getUserName() {
     return firebase.auth().currentUser.displayName;
@@ -68,32 +35,79 @@ firebase.auth().onAuthStateChanged(function(user) {
         button_logout.hidden = false;
         button_logout.innerText = `SIGN OUT, ${getUserName()}`;
         button_levels.disabled = false;
-        // var displayName = user.displayName;
-        // var email = user.email;
-        // var emailVerified = user.emailVerified;
-        // var photoURL = user.photoURL;
-        // var isAnonymous = user.isAnonymous;
-        // var uid = user.uid;
-        // var providerData = user.providerData;
     } else {
       // User is signed out.
         button_logout.hidden = true;
         button_login.hidden = false;
         button_levels.disabled = true;
+        imgWhereWaldo.src = 'images/waldo-original-image.png';
+        gameStarted = false;
+        if (gameId !== ''){
+            firebase.functions().httpsCallable('cancelGame')({gameId});
+        }
     }
 });
 
+async function fillBoardList(){
+    const querySnapshot = await firebase.firestore().collection('boards').get();
+    return querySnapshot.docs.map(doc => {return {name: doc.data().name, id: doc.id}});
+}
+
+function generateBoardButtons(parentDiv, boardList){
+    boardList.forEach(board => {
+        const divLevel = document.createElement('button');
+        divLevel.innerText = board.name;
+        divLevel.addEventListener('click', async (event) => {
+            popup.style.display = 'none';
+            try {
+                const newGame = await firebase.functions().httpsCallable('startGame')({boardId: board.id});
+                console.log(newGame);
+                imgWhereWaldo.src = newGame.subpath;
+                gameId = newGame.gameId;
+            } catch (error) {
+                alert(error.message);
+            }
+            gameStarted = true;
+        });
+        parentDiv.appendChild(divLevel);
+    });
+}
+
+async function generateLevelSelector(){
+    const divLevels = document.createElement('div');
+    divLevels.classList.toggle('white-rectangle');
+    const subtitle = document.createElement('h2');
+    subtitle.innerText = 'Select one of the levels: ';
+    divLevels.appendChild(subtitle);
+
+    try {
+        generateBoardButtons(divLevels, await fillBoardList());
+    } catch (error) {
+        alert(error.message);
+    }
+    return divLevels;
+}
+
 button_levels.addEventListener('click', async (event) => {
-    // popup.appendChild(await generateLevelSelector());
     popup.style.display = 'flex';
     popup.childNodes.forEach(child => child.remove());
     popup.appendChild(await generateLevelSelector());
 });
 
-document.querySelector('#img-where-waldo').addEventListener('click', event => {
+imgWhereWaldo.addEventListener('click', event => {
     // Odlaw widthRatio 0.2431640625 heightRatio 0.5026041666666666 main.js:2:13
     // Waldo widthRatio 0.53125 heightRatio 0.5026041666666666 main.js:2:13
     // Wizard widthRatio 0.62890625 heightRatio 0.5104166666666666 main.js:2:13
     // Wizard (the farthest from the center) radiuswidthRatio 0.6279296875 heightRatio 0.4713541666666667
     console.log(`widthRatio ${(event.pageX - event.target.offsetLeft)/event.target.clientWidth} heightRatio ${(event.pageY - event.target.offsetTop)/event.target.clientHeight}`)
+
+    if (gameStarted){
+        char_select.style.top = event.pageY+'px';
+        char_select.style.left = event.pageX+'px';
+        char_select.hidden = false;
+        if (charSelTimeout){
+            clearTimeout(charSelTimeout);
+        }
+        charSelTimeout = setTimeout(() => char_select.hidden = true, 5000);
+    }
 });
