f287bf04d0ee4aa7ea08cf30d39cda71902592a0
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-10-01 10:52:10 +0200

    Implement basic interface for using the clock

--- pomodoro-tracker/src/app/pomodoro.service.ts
+++ pomodoro-tracker/src/app/pomodoro.service.ts
@@ -3,15 +3,18 @@ import { Clock } from 'src/domain/Clock';
 import { CompletedPomodoro } from 'src/domain/CompletedPomodoro';
 import { Pomodoro } from 'src/domain/Pomodoro';
 import { Scheme } from 'src/domain/Scheme';
+import { Observer } from 'src/domain/Observer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PomodoroService implements Observer{
 
-  addedPomodoros = new Array<Pomodoro>();
+  // addedPomodoros = new Array<Pomodoro>();
   completedPomodoros = new Array<CompletedPomodoro>();
-  createdSchemes = [new Scheme(25 * 60, 5 * 60, 15 * 60, 4)];
+  // createdSchemes = [new Scheme(25 * 60, 5 * 60, 15 * 60, 4)];
+  addedPomodoros = [new Pomodoro('c1', 'First Pomodoro Twice', 2), new Pomodoro('c2', 'Second Pomodoro Thrice', 3)];
+  createdSchemes = [new Scheme(3 * 60, 1 * 60, 2 * 60, 3)];
   clock = new Clock(this.createdSchemes[0], this.addedPomodoros, this.completedPomodoros);
   currentTime = this.clock.getCurrentTime();
   isBreak = this.clock.scheme.isPeriodBreak();
