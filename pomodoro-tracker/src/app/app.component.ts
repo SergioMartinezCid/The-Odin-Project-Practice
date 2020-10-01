3ca524570951e51177836a78afc4204949884480
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-10-01 10:48:31 +0200

    Structure basic components

--- pomodoro-tracker/src/app/app.component.ts
+++ pomodoro-tracker/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pomodoro-tracker';
+  trackerVisible = true;
+  title = 'Pomodoro Tracker';
 }
--- pomodoro-tracker/src/app/app.module.ts
+++ pomodoro-tracker/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TrackerClockComponent } from './tracker-clock/tracker-clock.component';
+import { TrackerSectionComponent } from './tracker-section/tracker-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    TrackerComponent,
+    SettingsComponent,
+    TrackerClockComponent,
+    TrackerSectionComponent
   ],
   imports: [
     BrowserModule
