dad93eb50cd7ef32f3da0276e41fcddaf06ee6a0
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-11-11 13:51:15 +0100

    Implement genre_detail

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -7,8 +7,25 @@ exports.genre_list = async function(req, res) {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = async function(req, res, next) {
+    try {
+        const genre = await db.Genre.findByPk(parseInt(req.params.id));
+        const genre_books = await db.Book.findAll({
+            where: {
+                GenreId: parseInt(req.params.id)
+            }
+        });
+
+        if (genre == null){
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        
+        res.render('genre_detail', { title: 'Genre Detail', genre: genre, genre_books: genre_books } );
+    } catch (err) {
+        return next(err);
+    }
 };
 
 // Display Genre create form on GET.
