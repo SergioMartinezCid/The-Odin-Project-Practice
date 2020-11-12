bf3f42ad068c6a341a512234f13015a8cb69d2b3
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-11-12 09:50:55 +0100

    Add author_detail

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -7,8 +7,26 @@ exports.author_list = async function(req, res) {
 };
 
 // Display detail page for a specific Author.
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = async function(req, res, next) {
+    try {
+        const author = await db.Author.findByPk(parseInt(req.params.id));
+        const author_books = await db.Book.findAll({
+            attributes: ['id', 'title', 'summary'],
+            where: {
+                AuthorId: parseInt(req.params.id)
+            }
+        });
+
+        if (author == null) {
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('author_detail', { title: 'Author Detail', author: author, author_books: author_books });
+    } catch (err) {
+        return next(err);
+    } 
 };
 
 // Display Author create form on GET.
