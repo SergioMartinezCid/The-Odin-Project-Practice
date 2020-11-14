eb940d4e98f94741ea81ef400fd7639d2a86bded
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-11-14 16:55:51 +0100

    Implement update on book_form

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -1,25 +1,27 @@
-const { body,validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-exports.index = async function(req, res) {   
+exports.index = async function (req, res) {
     let err = false;
     let book_count, book_instance_count, book_instance_available_count,
         author_count, genre_count;
     try {
-       book_count = await db.Book.count();
-       book_instance_count = await db.BookInstance.count();
-       book_instance_available_count = await db.BookInstance.count({ where: [{'status': 'Available'}]});
-       author_count = await db.Author.count();
-       genre_count = await db.Genre.count();
+        book_count = await db.Book.count();
+        book_instance_count = await db.BookInstance.count();
+        book_instance_available_count = await db.BookInstance.count({ where: [{ 'status': 'Available' }] });
+        author_count = await db.Author.count();
+        genre_count = await db.Genre.count();
     } catch (error) {
         err = true;
     }
-    res.render('index', { title: 'Local Library Home', error: err, data: {
-        book_count, book_instance_count, book_instance_available_count, author_count, genre_count
-    } });
+    res.render('index', {
+        title: 'Local Library Home', error: err, data: {
+            book_count, book_instance_count, book_instance_available_count, author_count, genre_count
+        }
+    });
 };
 
 // Display list of all books.
-exports.book_list = async function(req, res, next) {
+exports.book_list = async function (req, res, next) {
     var list_books = await db.Book.findAll({
         attributes: ['id', 'title'],
         include: [{
@@ -31,7 +33,7 @@ exports.book_list = async function(req, res, next) {
 };
 
 // Display detail page for a specific book.
-exports.book_detail = async function(req, res, next) {
+exports.book_detail = async function (req, res, next) {
     try {
         const book = await db.Book.findOne({
             where: {
@@ -45,20 +47,20 @@ exports.book_detail = async function(req, res, next) {
             }
         });
 
-        if (book == null){
+        if (book == null) {
             var err = new Error('Book not found');
             err.status = 404;
             return next(err);
         }
-        
-        res.render('book_detail', { title: 'Book Detail', book: book, book_instances: book_instances } );
+
+        res.render('book_detail', { title: 'Book Detail', book: book, book_instances: book_instances });
     } catch (err) {
         return next(err);
     }
 };
 
 // Display book create form on GET.
-exports.book_create_get = async function(req, res) {
+exports.book_create_get = async function (req, res) {
     // Get all authors and genres, which we can use for adding to our book.
     try {
         const authors = await db.Author.findAll();
@@ -80,7 +82,7 @@ exports.book_create_post = [
 
     // Process request after validation and sanitization.
     async (req, res, next) => {
-        
+
         // Extract the validation errors from a request.
         const errors = validationResult(req);
 
@@ -119,21 +121,81 @@ exports.book_create_post = [
 ];
 
 // Display book delete form on GET.
-exports.book_delete_get = function(req, res) {
+exports.book_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Book delete GET');
 };
 
 // Handle book delete on POST.
-exports.book_delete_post = function(req, res) {
+exports.book_delete_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Book delete POST');
 };
 
 // Display book update form on GET.
-exports.book_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update GET');
+exports.book_update_get = async function (req, res, next) {
+    try {
+        const book = await db.Book.findOne({
+            include: [db.Author, db.Genre],
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        const authors = await db.Author.findAll();
+        const genres = await db.Genre.findAll();
+
+        if (book == null) { // No results.
+            var err = new Error('Book not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Success.
+        res.render('book_form', { title: 'Update Book', authors: authors, genres: genres, book: book });
+    } catch (err) {
+        return next(err);
+    }
 };
 
 // Handle book update on POST.
-exports.book_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update POST');
-};
+exports.book_update_post = [
+
+    // Validate and sanitise fields.
+    body('title', 'Title must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('author', 'Author must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('summary', 'Summary must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('isbn', 'ISBN must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('genre').escape(),
+
+    // Process request after validation and sanitization.
+    async (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+
+            // Get all authors and genres for form.
+            try {
+                const authors = await db.Author.findAll();
+                const genres = await db.Genre.findAll();
+                res.render('book_form', { title: 'Update Book', authors: authors, genres: genres, book: book, errors: errors.array() });
+                return;
+            } catch (err) {
+                return next(err);
+            }
+        } else {
+            // Data from form is valid. Update the record.
+            try {
+                const book = await db.Book.findByPk(parseInt(req.params.id));
+                book.title = req.body.title;
+                book.summary = req.body.summary;
+                book.isbn = req.body.isbn;
+                book.AuthorId = req.body.author;
+                book.GenreId = req.body.genre;
+                await book.save();
+                res.redirect(book.url);
+            } catch (err) {
+                return next(err);
+            }
+        }
+    }
+];
