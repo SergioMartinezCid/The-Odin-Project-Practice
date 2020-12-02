d5871c466af0c9d0a2a6dd218005b39dc061ec66
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-12-02 10:27:26 +0100

    Create ejs views

--- blog-api-top/app.js
+++ blog-api-top/app.js
@@ -11,6 +11,9 @@ passport = require('./passport');
 var app = express();
 
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
