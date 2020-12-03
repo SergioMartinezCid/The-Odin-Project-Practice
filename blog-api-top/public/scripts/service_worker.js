dc0aeacde32b537396843ad9f083cfc26bd85487
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-12-03 08:30:42 +0100

    Implement views

--- blog-api-top/public/scripts/service_worker.js
+++ blog-api-top/public/scripts/service_worker.js
@@ -1,7 +1,5 @@
-// TODO js history push para actualizar el historial cuando se hace window replace en el cliente
-
 self.addEventListener('fetch', async function (event) {
-        event.request.headers.append('Authorization', `Bearer ${sessionStorage.getItem('loginToken')}`);
+        event.request.headers.append('Authorization', `Bearer ${localStorage.getItem('loginToken')}`);
         const newRequest = new Request(event.request, {
             method: request.method,
             headers: request.headers,
--- blog-api-top/routes/reader.js
+++ blog-api-top/routes/reader.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('login', { title: 'Login Page' });
+    res.render('reader_homepage');
 });
 
 module.exports = router;
